d7230e818a3fd8a07383bfb556a54133c7461932
Victor <victormanuelnavarrocamino@gmail.com>
2023-12-06 11:14:31 +0100

    Creado contenidos

---
+++ entorno.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Examen ED</title>
+    <link rel="stylesheet" href="css/estilo.css">
+    <style>
+        div{
+            width: 600px;
+            margin: auto;
+        }
+    </style>
+
+</head>
+<body>
+    <div>
+        <h1>NOMBRE COMPLETO DEL ALUMNO</h1>
+        <img src="img/nieve.jpg" alt="Foto de la nieve">
+        <br>
+
+        <?php
+            echo "<h2>Examen Entorno de Desarrollo. Suerte!</h2>";
+        ?>
+    </div>
+</body>
+</html>
